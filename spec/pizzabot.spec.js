32cae1258c41134deb475f1b11e1e8af50797bf5
Brian Mullan <bmullan@spotx.tv>
2019-04-14 15:41:43 +0100

    2nd commit

--- spec/pizzabot.spec.js
+++ spec/pizzabot.spec.js
@@ -9,6 +9,7 @@ fdescribe('Given the Pizzabot', function() {
     it('Then it contains an add method', function() {
         // pizzabot.test("5x5(0,0)(1,3)(4,4)(4,2)(4,2)(0,1)(3,2)(2,3)(4,1)")
         pizzabot.deliveryInstructions("5x10(0,0)(2,2)(2,2)(70,1)(1,70)(1,1)(not)")
+        // pizzabot.deliveryInstructions("5x10(hello)")
         // expect(pizzabot.test("hello")).toBeDefined();
     });
 
